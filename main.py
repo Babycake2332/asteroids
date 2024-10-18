68b4b2f9b1af4597bf004ebae697e46a47baa1e1
Babycake2332 <skoog.alexander@gmail.com>
2024-10-18 11:02:39 +0200

    added basic score counter

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    font = pygame.font.SysFont("comicsans", 30, True)
 
 
     updatable = pygame.sprite.Group()
@@ -28,24 +29,36 @@ def main():
 
     dt = 0
 
+    score = 0
+    high_score = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+            
+        current_score = font.render("Score: " + str(score), 1, (255,255,255))
+        current_hscore = font.render("High Score: " + str(high_score), 1, (255,255,255))
+
         for object in updatable:
             object.update(dt)
 
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game over!")
+                if score > high_score:
+                    high_score = score
                 sys.exit()
-            for bullet in shots:
-                if asteroid.collides_with(bullet):
-                    bullet.kill()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
                     asteroid.split()
+                    score += 1
 
         screen.fill('black')
+        screen.blit(current_score, (390, 10))
+        screen.blit(current_hscore, (1090, 10))
 
         for object in drawable:
             object.draw(screen)
